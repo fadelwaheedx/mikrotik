2014aa41e5346d08a8ad6f125ceaf38126d83ef6
fadelwaheedx <fadel.waheed@gmail.com>
2025-11-27 00:25:14 +0200

    Fix typo in comments in Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -7,7 +7,7 @@
 |
 | The closure you provide to your test functions is always bound to a specific PHPUnit test
 | case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "uses()" function to bind a different classes or traits.
+| ne ed to change it using the "uses()" function to bind a different classes or traits.
 |
 */
 
